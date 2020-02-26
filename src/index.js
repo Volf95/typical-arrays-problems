1985876d1576cbbb9e1698537864b0bdca72f023
Laziz Khudayberdiev <57383456+Volf95@users.noreply.github.com>
2020-02-26 22:02:29 +0500

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length === 0) {
+    return 0;
+  }
+  let result = array.sort((a, b) => a - b);
+  return result[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length === 0) {
+    return 0;
+  }
+  let result = array.sort((a, b) => a - b);
+  return result[array.length - 1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+  let sortedArr = array.sort((a, b) => a - b);
+  for(let i = 0; i < sortedArr.length; i++){
+    sum +=  sortedArr[i];
+  }
+  return sum/sortedArr.length;
 }
